e58f2a3f2f74a1fe955ac07c9272d5597eba4b6c
MohsenSami <mohsensami88@gmail.com>
2023-03-23 00:10:12 +0330

    set loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import { Input, Grid, Icon, Container, Form, Image, Card, Header, Button } from "semantic-ui-react";
+import { Input, Grid, Icon, Container, Form, Image, Card, Header, Button, Loader, Dimmer } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 import { useState } from "react";
@@ -8,12 +8,14 @@ import { useState } from "react";
 function App() {
     const [username, setUsername] = useState([]);
     const [value, setValue] = useState("");
+    const [isloading, setIsloading] = useState(false);
     const submitHandler = (e) => {
-        console.log("submit");
+        setIsloading(true);
 
         fetch(`https://api.github.com/search/users?q=${value}`)
             .then((res) => res.json())
             .then((data) => setUsername([...data.items]));
+        setIsloading(false);
     };
 
     return (
@@ -22,38 +24,44 @@ function App() {
                 <Navbar />
                 <br />
                 <br />
-                <br />
                 <Header as="h2">Github Finder</Header>
+
                 <Form onSubmit={(e) => submitHandler(e)} className="">
                     <Form.Field>
                         <Input onChange={(e) => setValue(e.target.value)} label="@" placeholder="Github Username" />
                     </Form.Field>
+                    <Form.Button content="Submit" />
                 </Form>
                 <br />
-                <Grid>
-                    <Grid.Row columns={2}>
-                        {username
-                            ? username.map((item) => {
-                                  return (
-                                      <Grid.Column>
-                                          <Card>
-                                              <Image src={item.avatar_url} wrapped ui={false} />
-                                              <Card.Content>
-                                                  <Card.Header>{item.login}</Card.Header>
-                                              </Card.Content>
-                                              <Card.Content extra>
-                                                  <a>
-                                                      <Button href={item.html_url} content="Show" secondary />
-                                                  </a>
-                                              </Card.Content>
-                                          </Card>
-                                          <br />
-                                      </Grid.Column>
-                                  );
-                              })
-                            : null}
-                    </Grid.Row>
-                </Grid>
+
+                {isloading ? (
+                    <Loader active inline="centered" />
+                ) : (
+                    <Grid>
+                        <Grid.Row columns={2}>
+                            {username
+                                ? username.map((item) => {
+                                      return (
+                                          <Grid.Column key={item.id}>
+                                              <Card>
+                                                  <Image src={item.avatar_url} wrapped ui={false} />
+                                                  <Card.Content>
+                                                      <Card.Header>{item.login}</Card.Header>
+                                                  </Card.Content>
+                                                  <Card.Content extra>
+                                                      <a>
+                                                          <Button href={item.html_url} content="Show" secondary />
+                                                      </a>
+                                                  </Card.Content>
+                                              </Card>
+                                              <br />
+                                          </Grid.Column>
+                                      );
+                                  })
+                                : null}
+                        </Grid.Row>
+                    </Grid>
+                )}
             </Container>
         </>
     );
