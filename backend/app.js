09dc0b4ee5ffe72f915f50b421d56f69925157be
mohsensami <mohsensami18727@gmail.com>
2024-11-02 22:12:18 +0330

    Handle error in nodejs

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 var cors = require('cors');
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
 //database connection
 mongoose
@@ -16,6 +18,21 @@ mongoose
     })
     .then(() => console.log('DB connected'))
     .catch((err) => console.log(err));
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(
+    bodyParser.urlencoded({
+        limit: '5mb',
+        extended: true,
+    })
+);
+app.use(cookieParser());
+app.use(cors());
+
+// error middleware
+app.use(errorHandler);
 
 // port
 const port = process.env.port || 8000;
