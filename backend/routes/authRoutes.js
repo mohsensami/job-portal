b8fee6e0356b02e606f0983eb8cfc9ca15209765
mohsensami <mohsensami18727@gmail.com>
2024-11-04 20:19:08 +0330

    user signin controller and model mothods added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signup } = require('../controllers/authController');
+const { signup, signin } = require('../controllers/authController');
 const router = express.Router();
 
 //auth routes
 // api/signup
 router.post('/signup', signup);
+// api/signin
+router.post('/signin', signin);
 
 module.exports = router;
