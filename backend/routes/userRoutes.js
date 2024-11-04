c2d8e18aa520804f957887f72df69e2faf3c6331
mohsensami <mohsensami18727@gmail.com>
2024-11-04 22:54:36 +0330

    singleUser added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { allUsers } = require('../controllers/userController');
+const { allUsers, singleUser } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
 //user routes
 
 // /api/allusers
 router.get('/allusers', isAuthenticated, isAdmin, allUsers);
+// /api/user/id
+router.get('/user/:id', isAuthenticated, singleUser);
 
 module.exports = router;
