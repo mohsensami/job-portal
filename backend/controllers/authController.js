9fc2bda4aa13f80f253f58547f6f3a92bfa8cdfe
mohsensami <mohsensami18727@gmail.com>
2024-11-04 20:05:44 +0330

    user signup controller added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,19 @@
-exports.signin = (req, res) => {
-    res.send('Hello signin from Node Js');
+const User = require('../models/userModel');
+const ErrorResponse = require('../utils/errorResponse');
+
+exports.signup = async (req, res, next) => {
+    const { email } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        return next(new ErrorResponse('E-mail already registred', 400));
+    }
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            user,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
