a850d2fab6791591511dd63f1787a96a38eba56b
mohsensami <mohsensami18727@gmail.com>
2024-11-05 22:57:18 +0330

    Setup and config mui theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import { theme } from './theme';
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
         </>
     );
 }
