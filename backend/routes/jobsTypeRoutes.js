6c46b5e788324189d6051832956326b76edf4453
mohsensami <mohsensami18727@gmail.com>
2024-11-09 21:45:04 +0330

    deleteJobType added

--- backend/routes/jobsTypeRoutes.js
+++ backend/routes/jobsTypeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createJobType, allJobsType } = require('../controllers/jobsTypeController');
+const { createJobType, allJobsType, updateJobType, deleteJobType } = require('../controllers/jobsTypeController');
 const { isAuthenticated } = require('../middleware/auth');
 
 //job type routes
@@ -11,5 +11,7 @@ router.post('/type/create', isAuthenticated, createJobType);
 router.get('/type/jobs', allJobsType);
 // /api/type/update/type_id
 router.put('/type/update/:type_id', isAuthenticated, isAdmin, updateJobType);
+// /api/type/delete/type_id
+router.delete('/type/delete/:type_id', isAuthenticated, isAdmin, deleteJobType);
 
 module.exports = router;
