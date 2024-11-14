3e3cb231598f5ac4aae82360d1a31543dd357dd2
mohsensami <mohsensami18727@gmail.com>
2024-11-14 22:56:15 +0330

    dashboard page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,14 @@ import { theme } from './theme';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 import LogIn from './pages/LogIn';
 import UserDashboard from './pages/user/UserDashboard';
 import UserRoute from './component/UserRoute';
+import Layout from './pages/global/Layout';
+
+//HOC
+const UserDashboardHOC = Layout(UserDashboard);
 
 function App() {
     return (
@@ -16,23 +21,25 @@ function App() {
             <ToastContainer />
             <ThemeProvider theme={theme}>
                 <CssBaseline />
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/search/location/:location" element={<Home />} />
-                        <Route path="/search/:keyword" element={<Home />} />
-                        <Route path="/login" element={<LogIn />} />
-                        <Route
-                            path="/user/dashboard"
-                            element={
-                                <UserRoute>
-                                    <UserDashboard />
-                                </UserRoute>
-                            }
-                        />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
+                <ProSidebarProvider>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/search/location/:location" element={<Home />} />
+                            <Route path="/search/:keyword" element={<Home />} />
+                            <Route path="/login" element={<LogIn />} />
+                            <Route
+                                path="/user/dashboard"
+                                element={
+                                    <UserRoute>
+                                        <UserDashboardHOC />
+                                    </UserRoute>
+                                }
+                            />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </BrowserRouter>
+                </ProSidebarProvider>
             </ThemeProvider>
         </>
     );
