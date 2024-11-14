38b848b39c21c7f76d5cca91060d3b562e58de97
mohsensami <mohsensami18727@gmail.com>
2024-11-14 21:28:44 +0330

    404 notFound page updated

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
-function NotFound() {
-    return <div>NotFound</div>;
-}
+import { Box } from '@mui/material';
+import Footer from '../component/Footer';
+import Navbar from '../component/Navbar';
+
+const NotFound = () => {
+    return (
+        <>
+            <Navbar />
+            <Box sx={{ height: '81vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <h1>Page not found!</h1>
+            </Box>
+            <Footer />
+        </>
+    );
+};
 
 export default NotFound;
