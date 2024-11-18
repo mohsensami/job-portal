53ce9cc89893b8a7de8acada10a11148d976d9d0
mohsensami <mohsensami18727@gmail.com>
2024-11-18 18:11:39 +0330

    DashJob page added

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -11,17 +11,16 @@ exports.createJob = async (req, res, next) => {
             salary: req.body.salary,
             location: req.body.location,
             jobType: req.body.jobType,
-            user: req.user.id
+            user: req.user.id,
         });
         res.status(201).json({
             success: true,
-            job
-        })
+            job,
+        });
     } catch (error) {
         next(error);
     }
-}
-
+};
 
 //single job
 exports.singleJob = async (req, res, next) => {
@@ -29,61 +28,59 @@ exports.singleJob = async (req, res, next) => {
         const job = await Job.findById(req.params.id);
         res.status(200).json({
             success: true,
-            job
-        })
+            job,
+        });
     } catch (error) {
         next(error);
     }
-}
-
+};
 
 //update job by id.
 exports.updateJob = async (req, res, next) => {
     try {
-        const job = await Job.findByIdAndUpdate(req.params.job_id, req.body, { new: true }).populate('jobType', 'jobTypeName').populate('user', 'firstName lastName');
+        const job = await Job.findByIdAndUpdate(req.params.job_id, req.body, { new: true })
+            .populate('jobType', 'jobTypeName')
+            .populate('user', 'firstName lastName');
         res.status(200).json({
             success: true,
-            job
-        })
+            job,
+        });
     } catch (error) {
         next(error);
     }
-}
-
+};
 
 //update job by id.
 exports.showJobs = async (req, res, next) => {
-
-    //enable search 
-    const keyword = req.query.keyword ? {
-        title: {
-            $regex: req.query.keyword,
-            $options: 'i'
-        }
-    } : {}
-
+    //enable search
+    const keyword = req.query.keyword
+        ? {
+              title: {
+                  $regex: req.query.keyword,
+                  $options: 'i',
+              },
+          }
+        : {};
 
     // filter jobs by category ids
     let ids = [];
     const jobTypeCategory = await JobType.find({}, { _id: 1 });
-    jobTypeCategory.forEach(cat => {
+    jobTypeCategory.forEach((cat) => {
         ids.push(cat._id);
-    })
+    });
 
     let cat = req.query.cat;
     let categ = cat !== '' ? cat : ids;
 
-
     //jobs by location
     let locations = [];
     const jobByLocation = await Job.find({}, { location: 1 });
-    jobByLocation.forEach(val => {
+    jobByLocation.forEach((val) => {
         locations.push(val.location);
     });
     let setUniqueLocation = [...new Set(locations)];
     let location = req.query.location;
     let locationFilter = location !== '' ? location : setUniqueLocation;
-
 
     //enable pagination
     const pageSize = 5;
@@ -92,22 +89,21 @@ exports.showJobs = async (req, res, next) => {
     const count = await Job.find({ ...keyword, jobType: categ, location: locationFilter }).countDocuments();
 
     try {
-        const jobs = await Job.find({ ...keyword, jobType: categ, location: locationFilter }).sort({ createdAt: -1 }).skip(pageSize * (page - 1)).limit(pageSize)
+        const jobs = await Job.find({ ...keyword, jobType: categ, location: locationFilter })
+            .sort({ createdAt: -1 })
+            .populate('jobType', 'jobTypeName')
+            .populate('user', 'firstName')
+            .skip(pageSize * (page - 1))
+            .limit(pageSize);
         res.status(200).json({
             success: true,
             jobs,
             page,
             pages: Math.ceil(count / pageSize),
             count,
-            setUniqueLocation
-
-        })
+            setUniqueLocation,
+        });
     } catch (error) {
         next(error);
     }
-}
-
-
-
-
-
+};
