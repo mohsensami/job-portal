911079193b2263007cf5e897e3fa5613d2e1e35e
mohsensami <mohsensami18727@gmail.com>
2025-12-01 21:47:32 +0330

    update baseApiUrl

--- frontend/service/api.js
+++ frontend/service/api.js
@@ -1,37 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 
-
+const apiURL =
+  import.meta.env.VITE_BASE_URL ||
+  "http://localhost:8000" ||
+  "https://job-portal-backend.vercel.app";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    // timeout: 5000, // Optional timeout value in milliseconds
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: apiURL,
+  // timeout: 5000, // Optional timeout value in milliseconds
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        console.log(token);
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        Promise.reject(error);
+  (config) => {
+    console.log(token);
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-    async (response) => {
-        return response;
-    },
-    (error) => {
-        // toast.error(error.message);
+  async (response) => {
+    return response;
+  },
+  (error) => {
+    // toast.error(error.message);
 
-        return Promise.reject(error);
-    }
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
