6a6a4b5864f4918a7b5eb0cb75ff374a8ec39bb6
mohsensami <mohsensami18727@gmail.com>
2025-12-01 22:12:45 +0330

    update axiosInstance

--- frontend/service/api.js
+++ frontend/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiURL =
   import.meta.env.VITE_BASE_URL || "https://job-portal-j3fi.onrender.com";
 const axiosInstance = axios.create({
-  baseURL: apiURL,
+  baseURL: "https://job-portal-j3fi.onrender.com",
   // timeout: 5000, // Optional timeout value in milliseconds
   headers: {
     "Content-Type": "application/json",
